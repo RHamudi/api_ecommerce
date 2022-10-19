20b60b44f347371b696463cddbd3f2b29748a283
RHamudi <ramonramos.silva16@gmail.com>
2022-10-19 20:54:40 -0300

    feat: user model

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
   username: {
-    type: Strig,
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
     required: true,
   },
   password: {
