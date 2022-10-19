7495c22f219712a6fa1a8433494c187056bbd67c
RHamudi <ramonramos.silva16@gmail.com>
2022-10-19 17:52:09 -0300

    feat: create product schema

---
+++ src/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
+  productDescription: {
+    type: String,
+    required: true,
+  },
+  productPrice: {
+    type: Number,
+    required: true,
+  },
+  productQuantity: {
+    type: Number,
+    required: true,
+  },
+  productImage: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Product", Schema);
