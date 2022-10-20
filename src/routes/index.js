90d445f5c1ba866bf70cb1b1e1a583dec1bb5b16
RHamudi <ramonramos.silva16@gmail.com>
2022-10-20 15:50:34 -0300

    fix: route name

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,35 +8,31 @@ const { authenticate } = require("../middlewares/index");
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  res.send("olá mundo");
-});
-
 routes.post("/create/user", UserController.createUser);
-routes.get("/users", UserController.getUsers);
+routes.get("/list/users", UserController.getUsers);
 
-routes.get("/users/:user_id", UserController.getUserById);
+routes.get("/list/usersid/:user_id", UserController.getUserById);
 
 routes.post("/login", LoginController.createSession);
 
 routes.post("/create/product/:user_id", authenticate, ProductController.createProduct);
-routes.get("/products/:user_id", ProductController.getUserProducts);
+routes.get("/list/productsuser/:user_id", ProductController.getUserProducts);
 routes.patch(
-  "/product/:user_id/:product_id",
+  "/update/product/:user_id/:product_id",
   authenticate,
   ProductController.updateProduct
 );
 routes.delete(
-  "/product/:user_id/:product_id",
+  "/delete/product/:user_id/:product_id",
   authenticate,
   ProductController.deleteProduct
 );
 
-routes.get("/products", ProductController.getProducts);
-routes.get("/product/:product_id", ProductController.getProductById);
+routes.get("/list/products", ProductController.getProducts);
+routes.get("/list/productid/:product_id", ProductController.getProductById);
 
-routes.post("/cart/:user_id", authenticate, CartController.createCart);
-routes.get("/cart/:user_id", authenticate, CartController.getUserCart);
+routes.post("/create/cart/:user_id", authenticate, CartController.createCart);
+routes.get("/list/cartuser/:user_id", authenticate, CartController.getUserCart);
 routes.get("/cart/:user_id/:cart_id", authenticate, CartController.getCart);
 
 module.exports = routes;
