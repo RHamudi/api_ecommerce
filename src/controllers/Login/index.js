dd2cea97585c851643eaf39208d756937bbce173
RHamudi <ramonramos.silva16@gmail.com>
2022-10-20 15:48:22 -0300

    feat: login validations

--- src/controllers/Login/index.js
+++ src/controllers/Login/index.js
@@ -1,12 +1,28 @@
 const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const LoginController = {
   async createSession(req, res) {
     const { email, password } = req.body;
 
+    // find email exists
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ erro: "Usuario não encontrado" });
+    // confirm password
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) return res.status(422).json({ erro: "senha Invalida" });
     try {
-      const user = await User.findOne({ email, password });
-      return res.status(200).json(user);
+      const secret = process.env.SECRET;
+
+      const token = jwt.sign({ id: user.id }, secret, { expiresIn: 1800 });
+      return res.status(200).json({
+        msg: "Autenticação realizada com sucesso",
+        username: user.username,
+        id: user.id,
+        token,
+      });
     } catch (error) {
       return res.status(400).json(error);
     }
