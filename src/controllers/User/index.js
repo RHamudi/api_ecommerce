3576273a716f25f736e5423b49e86c27ab6b759d
RHamudi <ramonramos.silva16@gmail.com>
2022-10-20 15:49:39 -0300

    feat: user validations

--- src/controllers/User/index.js
+++ src/controllers/User/index.js
@@ -1,13 +1,33 @@
 const User = require("../../models/User");
+const bcrypt = require("bcrypt");
 
 const UserController = {
   //Criar usuario
   async createUser(req, res) {
-    const bodyData = req.body;
+    const { username, email, password } = req.body;
+    if (!username) return res.status(400).json({ erro: "O nome é obrigatorio" });
+    if (!email) return res.status(400).json({ erro: "O E-mail é obrigatorio" });
+    if (!password) return res.status(400).json({ erro: "A senha é obrigatoria" });
+
+    // check user exists
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) return res.status(422).json({ erro: "O usuario já existe" });
+
+    // create password
+    const salt = await bcrypt.genSalt(15);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
 
     try {
-      const newUser = await User.create(bodyData);
-      res.status(200).json(newUser);
+      await user.save();
+      return res.status(200).json({ msg: "Usuario criado com sucesso!" });
     } catch (error) {
       return res.status(400).json({ error: error });
     }
@@ -16,7 +36,13 @@ const UserController = {
   async getUsers(req, res) {
     try {
       const users = await User.find();
-      return res.status(200).json(users);
+      return res.status(200).json(
+        users.map((item) => ({
+          id: item.id,
+          username: item.username,
+          email: item.email,
+        }))
+      );
     } catch (error) {
       return res.status(400).json(error);
     }
